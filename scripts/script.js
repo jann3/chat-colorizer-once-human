812f45fdd6a093270756d1de33441a29c1cabf97
Jann3 <jannecleary@hotmail.com>
2024-07-21 22:40:14 +0100

    Optmised load order

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+const backgroundImages = [
+    'images/once-human-background-low-res1.jpg',
+    'images/once-human-background-low-res2.jpg',
+    'images/once-human-background-low-res3.jpg',
+    'images/once-human-background-low-res4.jpg'
+];
+
+function setRandomBackgroundImage() {
+    const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+    document.body.style.backgroundImage = `url(${backgroundImages[randomIndex]})`;
+}
+
+setRandomBackgroundImage();
 
 const colorPicker = document.getElementById('color-picker');
 const messageInput = document.getElementById('message-input');
@@ -105,17 +118,4 @@ function copyCode() {
     }
 }
 
-const backgroundImages = [
-    'images/once-human-background-low-res1.jpg',
-    'images/once-human-background-low-res2.jpg',
-    'images/once-human-background-low-res3.jpg',
-    'images/once-human-background-low-res4.jpg'
-];
-
-function setRandomBackgroundImage() {
-    const randomIndex = Math.floor(Math.random() * backgroundImages.length);
-    document.body.style.backgroundImage = `url(${backgroundImages[randomIndex]})`;
-}
-
-window.addEventListener('load', setRandomBackgroundImage);
 messageInput.addEventListener('input', updateOutputCode);
