9f077a8b296df0a5b6debd4d1f76637bca3c1e95
Jann3 <jannecleary@hotmail.com>
2024-07-27 04:48:07 +0100

    Changed background image paths to webp versions and function to function expression

--- scripts/background-image-script.js
+++ scripts/background-image-script.js
@@ -1,15 +1,15 @@
 const backgroundImages = [
-    'images/once-human-background-low-res1.jpg',
-    'images/once-human-background-low-res2.jpg',
-    'images/once-human-background-low-res3.jpg',
-    'images/once-human-background-low-res4.jpg'
+    'images/once-human-background-low-res1.webp',
+    'images/once-human-background-low-res2.webp',
+    'images/once-human-background-low-res3.webp',
+    'images/once-human-background-low-res4.webp'
 ];
 
-function setRandomBackgroundImage() {
+const setRandomBackgroundImage = () => {
     if (document.documentElement) {
         const randomIndex = Math.floor(Math.random() * backgroundImages.length);
         document.body.style.backgroundImage = `url(${backgroundImages[randomIndex]})`;
     }
-}
+};
 
 setRandomBackgroundImage();
